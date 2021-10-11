cbbfd512170baee7c02c07d7e74de81107451fb9
Mohammed <mohammed.rabee312@gmail.com>
2021-10-11 20:44:42 +0300

    everything_work_start_UI

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.dashboard')
+
+@section('content')
+<div class="content">
+    <div class="container-fluid">
+      <div class="row">
+          <h4 class="box-title">Welcome Again </h4> 
+          {{-- {{Auth::user()->fname}} --}}
+      </div>
+    </div>
+  </div>
+@endsection
